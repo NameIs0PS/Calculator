e59c86e98ad628dbab2fb899020d1eb72e3bc193
NameIs0PS <ekaterina_jhb_2111@mail.ru>
2025-02-05 19:59:08 +0500

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,313 @@
+﻿namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtDisplay = new System.Windows.Forms.TextBox();
+            this.btn7 = new System.Windows.Forms.Button();
+            this.btn8 = new System.Windows.Forms.Button();
+            this.btn9 = new System.Windows.Forms.Button();
+            this.btn4 = new System.Windows.Forms.Button();
+            this.btn5 = new System.Windows.Forms.Button();
+            this.btn6 = new System.Windows.Forms.Button();
+            this.btn1 = new System.Windows.Forms.Button();
+            this.btn2 = new System.Windows.Forms.Button();
+            this.btn3 = new System.Windows.Forms.Button();
+            this.btnDivide = new System.Windows.Forms.Button();
+            this.btnMultiply = new System.Windows.Forms.Button();
+            this.btnSubtract = new System.Windows.Forms.Button();
+            this.btnClear = new System.Windows.Forms.Button();
+            this.btn0 = new System.Windows.Forms.Button();
+            this.btnEquals = new System.Windows.Forms.Button();
+            this.btnAdd = new System.Windows.Forms.Button();
+            this.btnSave = new System.Windows.Forms.Button();
+            this.chkDarkTheme = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // txtDisplay
+            // 
+            this.txtDisplay.Location = new System.Drawing.Point(12, 12);
+            this.txtDisplay.Name = "txtDisplay";
+            this.txtDisplay.Size = new System.Drawing.Size(118, 20);
+            this.txtDisplay.TabIndex = 0;
+            // 
+            // btn7
+            // 
+            this.btn7.Location = new System.Drawing.Point(12, 38);
+            this.btn7.Name = "btn7";
+            this.btn7.Size = new System.Drawing.Size(25, 25);
+            this.btn7.TabIndex = 1;
+            this.btn7.Text = "7";
+            this.btn7.UseVisualStyleBackColor = true;
+            this.btn7.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btn8
+            // 
+            this.btn8.Location = new System.Drawing.Point(43, 38);
+            this.btn8.Name = "btn8";
+            this.btn8.Size = new System.Drawing.Size(25, 25);
+            this.btn8.TabIndex = 2;
+            this.btn8.Text = "8";
+            this.btn8.UseVisualStyleBackColor = true;
+            this.btn8.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btn9
+            // 
+            this.btn9.Location = new System.Drawing.Point(74, 38);
+            this.btn9.Name = "btn9";
+            this.btn9.Size = new System.Drawing.Size(25, 25);
+            this.btn9.TabIndex = 3;
+            this.btn9.Text = "9";
+            this.btn9.UseVisualStyleBackColor = true;
+            this.btn9.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btn4
+            // 
+            this.btn4.Location = new System.Drawing.Point(12, 69);
+            this.btn4.Name = "btn4";
+            this.btn4.Size = new System.Drawing.Size(25, 25);
+            this.btn4.TabIndex = 4;
+            this.btn4.Text = "4";
+            this.btn4.UseVisualStyleBackColor = true;
+            this.btn4.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btn5
+            // 
+            this.btn5.Location = new System.Drawing.Point(43, 69);
+            this.btn5.Name = "btn5";
+            this.btn5.Size = new System.Drawing.Size(25, 25);
+            this.btn5.TabIndex = 5;
+            this.btn5.Text = "5";
+            this.btn5.UseVisualStyleBackColor = true;
+            this.btn5.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btn6
+            // 
+            this.btn6.Location = new System.Drawing.Point(74, 69);
+            this.btn6.Name = "btn6";
+            this.btn6.Size = new System.Drawing.Size(25, 25);
+            this.btn6.TabIndex = 6;
+            this.btn6.Text = "6";
+            this.btn6.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btn6.UseVisualStyleBackColor = true;
+            this.btn6.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btn1
+            // 
+            this.btn1.Location = new System.Drawing.Point(12, 100);
+            this.btn1.Name = "btn1";
+            this.btn1.Size = new System.Drawing.Size(25, 25);
+            this.btn1.TabIndex = 7;
+            this.btn1.Text = "1";
+            this.btn1.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btn1.UseVisualStyleBackColor = true;
+            this.btn1.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btn2
+            // 
+            this.btn2.BackColor = System.Drawing.Color.Transparent;
+            this.btn2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.btn2.FlatAppearance.BorderColor = System.Drawing.SystemColors.Control;
+            this.btn2.FlatAppearance.BorderSize = 0;
+            this.btn2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.btn2.Location = new System.Drawing.Point(43, 100);
+            this.btn2.Name = "btn2";
+            this.btn2.Size = new System.Drawing.Size(25, 25);
+            this.btn2.TabIndex = 8;
+            this.btn2.Text = "2";
+            this.btn2.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btn2.UseVisualStyleBackColor = false;
+            this.btn2.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btn3
+            // 
+            this.btn3.Location = new System.Drawing.Point(74, 100);
+            this.btn3.Name = "btn3";
+            this.btn3.Size = new System.Drawing.Size(25, 25);
+            this.btn3.TabIndex = 9;
+            this.btn3.Text = "3";
+            this.btn3.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btn3.UseVisualStyleBackColor = true;
+            this.btn3.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btnDivide
+            // 
+            this.btnDivide.Location = new System.Drawing.Point(105, 38);
+            this.btnDivide.Name = "btnDivide";
+            this.btnDivide.Size = new System.Drawing.Size(25, 25);
+            this.btnDivide.TabIndex = 10;
+            this.btnDivide.Text = "/";
+            this.btnDivide.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnDivide.UseVisualStyleBackColor = true;
+            this.btnDivide.Click += new System.EventHandler(this.btnDivide_Click);
+            // 
+            // btnMultiply
+            // 
+            this.btnMultiply.Location = new System.Drawing.Point(105, 69);
+            this.btnMultiply.Name = "btnMultiply";
+            this.btnMultiply.Size = new System.Drawing.Size(25, 25);
+            this.btnMultiply.TabIndex = 11;
+            this.btnMultiply.Text = "*";
+            this.btnMultiply.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnMultiply.UseVisualStyleBackColor = true;
+            this.btnMultiply.Click += new System.EventHandler(this.btnMultiply_Click);
+            // 
+            // btnSubtract
+            // 
+            this.btnSubtract.Location = new System.Drawing.Point(105, 100);
+            this.btnSubtract.Name = "btnSubtract";
+            this.btnSubtract.Size = new System.Drawing.Size(25, 25);
+            this.btnSubtract.TabIndex = 12;
+            this.btnSubtract.Text = "-";
+            this.btnSubtract.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnSubtract.UseVisualStyleBackColor = true;
+            this.btnSubtract.Click += new System.EventHandler(this.btnSubtract_Click);
+            // 
+            // btnClear
+            // 
+            this.btnClear.Location = new System.Drawing.Point(12, 131);
+            this.btnClear.Name = "btnClear";
+            this.btnClear.Size = new System.Drawing.Size(25, 25);
+            this.btnClear.TabIndex = 13;
+            this.btnClear.Text = "C";
+            this.btnClear.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnClear.UseVisualStyleBackColor = true;
+            this.btnClear.Click += new System.EventHandler(this.btnClear_Click);
+            // 
+            // btn0
+            // 
+            this.btn0.Location = new System.Drawing.Point(43, 131);
+            this.btn0.Name = "btn0";
+            this.btn0.Size = new System.Drawing.Size(25, 25);
+            this.btn0.TabIndex = 14;
+            this.btn0.Text = "0";
+            this.btn0.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btn0.UseVisualStyleBackColor = true;
+            this.btn0.Click += new System.EventHandler(this.btnNumber_Click);
+            // 
+            // btnEquals
+            // 
+            this.btnEquals.Location = new System.Drawing.Point(74, 131);
+            this.btnEquals.Name = "btnEquals";
+            this.btnEquals.Size = new System.Drawing.Size(25, 25);
+            this.btnEquals.TabIndex = 15;
+            this.btnEquals.Text = "=";
+            this.btnEquals.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnEquals.UseVisualStyleBackColor = true;
+            this.btnEquals.Click += new System.EventHandler(this.btnEquals_Click);
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.Location = new System.Drawing.Point(105, 131);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.Size = new System.Drawing.Size(25, 25);
+            this.btnAdd.TabIndex = 16;
+            this.btnAdd.Text = "+";
+            this.btnAdd.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnAdd.UseVisualStyleBackColor = true;
+            this.btnAdd.Click += new System.EventHandler(this.btnAdd_Click);
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(12, 162);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(118, 25);
+            this.btnSave.TabIndex = 17;
+            this.btnSave.Text = "Сохранить в файл";
+            this.btnSave.TextImageRelation = System.Windows.Forms.TextImageRelation.TextAboveImage;
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.btnSave_Click);
+            // 
+            // chkDarkTheme
+            // 
+            this.chkDarkTheme.AutoSize = true;
+            this.chkDarkTheme.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.chkDarkTheme.Location = new System.Drawing.Point(12, 197);
+            this.chkDarkTheme.Name = "chkDarkTheme";
+            this.chkDarkTheme.Size = new System.Drawing.Size(93, 17);
+            this.chkDarkTheme.TabIndex = 19;
+            this.chkDarkTheme.Text = "Тёмная тема";
+            this.chkDarkTheme.TextImageRelation = System.Windows.Forms.TextImageRelation.TextBeforeImage;
+            this.chkDarkTheme.UseVisualStyleBackColor = true;
+            this.chkDarkTheme.CheckedChanged += new System.EventHandler(this.chkDarkTheme_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(154, 226);
+            this.Controls.Add(this.chkDarkTheme);
+            this.Controls.Add(this.btnSave);
+            this.Controls.Add(this.btnAdd);
+            this.Controls.Add(this.btnEquals);
+            this.Controls.Add(this.btn0);
+            this.Controls.Add(this.btnClear);
+            this.Controls.Add(this.btnSubtract);
+            this.Controls.Add(this.btnMultiply);
+            this.Controls.Add(this.btnDivide);
+            this.Controls.Add(this.btn3);
+            this.Controls.Add(this.btn2);
+            this.Controls.Add(this.btn1);
+            this.Controls.Add(this.btn6);
+            this.Controls.Add(this.btn5);
+            this.Controls.Add(this.btn4);
+            this.Controls.Add(this.btn9);
+            this.Controls.Add(this.btn8);
+            this.Controls.Add(this.btn7);
+            this.Controls.Add(this.txtDisplay);
+            this.Name = "Form1";
+            this.Text = "Калькулятор";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtDisplay;
+        private System.Windows.Forms.Button btn7;
+        private System.Windows.Forms.Button btn8;
+        private System.Windows.Forms.Button btn9;
+        private System.Windows.Forms.Button btn4;
+        private System.Windows.Forms.Button btn5;
+        private System.Windows.Forms.Button btn6;
+        private System.Windows.Forms.Button btn1;
+        private System.Windows.Forms.Button btn2;
+        private System.Windows.Forms.Button btn3;
+        private System.Windows.Forms.Button btnDivide;
+        private System.Windows.Forms.Button btnMultiply;
+        private System.Windows.Forms.Button btnSubtract;
+        private System.Windows.Forms.Button btnClear;
+        private System.Windows.Forms.Button btn0;
+        private System.Windows.Forms.Button btnEquals;
+        private System.Windows.Forms.Button btnAdd;
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.CheckBox chkDarkTheme;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        private double currentValue = 0;
+        private double previousValue = 0;
+        private string operation = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnNumber_Click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            txtDisplay.Text += button.Text; // Добавляем нажатую цифру в дисплей
+        }
+
+        private void btnAdd_Click(object sender, EventArgs e) => SetOperation("+");
+        private void btnSubtract_Click(object sender, EventArgs e) => SetOperation("-");
+        private void btnMultiply_Click(object sender, EventArgs e) => SetOperation("*");
+        private void btnDivide_Click(object sender, EventArgs e) => SetOperation("/");
+
+        private void SetOperation(string op)
+        {
+            // Сохраняем текущее значение из текстового поля
+            if (!string.IsNullOrEmpty(txtDisplay.Text))
+            {
+                previousValue = double.Parse(txtDisplay.Text);
+            }
+
+            operation = op; // Устанавливаем текущую операцию
+            txtDisplay.Clear(); // Очищаем дисплей для ввода следующего числа
+        }
+
+        private void btnEquals_Click(object sender, EventArgs e)
+        {
+            switch (operation)
+            {
+                case "+":
+                    currentValue = previousValue + double.Parse(txtDisplay.Text);
+                    break;
+                case "-":
+                    currentValue = previousValue - double.Parse(txtDisplay.Text);
+                    break;
+                case "*":
+                    currentValue = previousValue * double.Parse(txtDisplay.Text);
+                    break;
+                case "/":
+                    if (double.Parse(txtDisplay.Text) == 0)
+                    {
+                        MessageBox.Show("Ошибка: Деление на ноль!");
+                        return;
+                    }
+                    currentValue = previousValue / double.Parse(txtDisplay.Text);
+                    break;
+            }
+            txtDisplay.Text = currentValue.ToString();
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            txtDisplay.Clear();
+            currentValue = 0;
+            previousValue = 0;
+            operation = "";
+        }
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            File.AppendAllText("results.txt", $"{DateTime.Now}: {currentValue}\n");
+            MessageBox.Show("Результат сохранен!");
+        }
+        private void chkDarkTheme_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chkDarkTheme.Checked) // Если флажок установлен
+            {
+                this.BackColor = System.Drawing.Color.Black; // Устанавливаем темный фон формы
+                txtDisplay.BackColor = System.Drawing.Color.Gray; // Устанавливаем темный фон для TextBox
+                txtDisplay.ForeColor = System.Drawing.Color.White; // Устанавливаем белый текст для TextBox
+                chkDarkTheme.ForeColor = System.Drawing.Color.White; 
+                // Устанавливаем цвет для всех кнопок
+                foreach (Control control in this.Controls)
+                {
+                    if (control is Button button)
+                    {
+                        button.BackColor = System.Drawing.Color.Maroon; // Темный фон для кнопок
+                        button.ForeColor = System.Drawing.Color.White; // Белый текст для кнопок
+                    }
+                }
+            }
+            else // Если флажок снят
+            {
+                this.BackColor = System.Drawing.Color.White; 
+                txtDisplay.BackColor = System.Drawing.Color.LightGray; 
+                txtDisplay.ForeColor = System.Drawing.Color.Black; 
+                chkDarkTheme.ForeColor = System.Drawing.Color.Black;
+                // Устанавливаем цвет для всех кнопок
+                foreach (Control control in this.Controls)
+                {
+                    if (control is Button button)
+                    {
+                        button.BackColor = System.Drawing.Color.LightGray; 
+                        button.ForeColor = System.Drawing.Color.Black; 
+                    }
+                }
+            }
+        }
+    }
+}
